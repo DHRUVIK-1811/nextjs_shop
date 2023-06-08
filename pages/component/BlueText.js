0b3d2d074e45e0f341520338e75ea20be7c8e4cf
DHRUVIK-1811 <dhruvik1811@gmail.com>
2023-06-08 17:43:43 +0530

    first

---
+++ pages/component/BlueText.js
@@ -0,0 +1,17 @@
+const BlueText = () => {
+    
+  return (
+    <>
+      <div>
+        <section>
+          <div class="content">
+            <h2>Dhruvik</h2>
+            <h2>Dhruvik</h2>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default BlueText;
---
+++ pages/component/GreenText.js
@@ -0,0 +1,26 @@
+
+const GreenText = () => {
+  return (
+      <div className="loader">
+        <span>D</span>
+        <span>h</span>
+        <span>r</span>
+        <span>u</span>
+        <span>v</span>
+        <span>i</span>
+        <span>k</span>
+        <span>_</span>
+        <span>V</span>
+        <span>a</span>
+        <span>g</span>
+        <span>h</span>
+        <span>a</span>
+        <span>s</span>
+        <span>i</span>
+        <span>y</span>
+        <span>a</span>
+      </div>
+  )
+}
+
+export default GreenText
